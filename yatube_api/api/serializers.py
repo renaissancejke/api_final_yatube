1244bd44fea84d434818009a9ba33d952ba1ada8
renaissance <rkrasnoshchekov@gmail.com>
2024-04-11 17:33:36 +0300

    commit_4

--- yatube_api/api/serializers.py
+++ yatube_api/api/serializers.py
@@ -6,36 +6,43 @@ from posts.models import Comment, Post, Group, Follow, User
 
 
 class PostSerializer(serializers.ModelSerializer):
-    author = SlugRelatedField(slug_field='username', read_only=True)
+    author = SlugRelatedField(
+        slug_field='username',
+        read_only=True
+    )
 
     class Meta:
-        fields = '__all__'
         model = Post
+        fields = '__all__'
+        read_only_fields = ('following',)
 
 
 class CommentSerializer(serializers.ModelSerializer):
     author = serializers.SlugRelatedField(
-        read_only=True, slug_field='username'
+        slug_field='username',
+        read_only=True
     )
 
     class Meta:
-        fields = '__all__'
         model = Comment
+        fields = '__all__'
+        read_only_fields = ('author', 'post',)
 
 
 class FollowSerializer(serializers.ModelSerializer):
     user = serializers.SlugRelatedField(
         slug_field='username',
-        default=serializers.CurrentUserDefault(),
-        queryset=User.objects.all()
+        read_only=True,
+        default=serializers.CurrentUserDefault()
     )
     following = serializers.SlugRelatedField(
-        slug_field='username', queryset=User.objects.all()
+        slug_field='username',
+        queryset=User.objects.all(),
     )
 
     class Meta:
-        fields = '__all__'
         model = Follow
+        exclude = ('id',)
         validators = [
             UniqueTogetherValidator(
                 queryset=Follow.objects.all(),
@@ -51,6 +58,7 @@ class FollowSerializer(serializers.ModelSerializer):
 
 
 class GroupSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = Group
         fields = '__all__'
